cb3690b708767d233dbcf9132c45c9f43b3e30b4
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-23 19:31:58 +0530

    Writes IncrementOperatorDemo

---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,14 @@
+class IncrementOperatorDemo{
+public static void main(String[] args){
+System.out.println("Pre increment operation----");
+int a=1;
+int b=++a;
+System.out.println("a: "+a);
+System.out.println("b: "+b);
+System.out.println("Post increment operation----");
+int i=100;
+int j=i++;
+System.out.println("i: "+i);
+System.out.println("j: "+j);
+}
+}
