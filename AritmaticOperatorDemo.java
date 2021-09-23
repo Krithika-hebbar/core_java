44a54ef74727c7cb25f21e42d28550b3d05ce9c4
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-23 19:27:17 +0530

    Writes AritmaticOperatorDemo program

---
+++ AritmaticOperatorDemo.java
@@ -0,0 +1,24 @@
+class AritmaticOperatorDemo{
+public static void main(String[] args){
+int a=10;
+int b=20;
+int addResult=a+b;
+System.out.println("addResult: "+addResult);
+int x=100;
+int y=50;
+int subResult=x-y;
+System.out.println("subResult: "+subResult);
+int p=5;
+int q=10;
+int mulResult=p*q;
+System.out.println("mulResult: "+mulResult);
+int i=10;
+int j=5;
+int divResult=j/i;
+System.out.println("divResult: "+divResult);
+int k=7;
+int l=2;
+int modResult=k%l;
+System.out.println("modResult: "+modResult);
+}
+}
