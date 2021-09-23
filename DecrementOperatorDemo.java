329998086fc92861cd68fc03a4fcf85772174a01
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-23 19:46:55 +0530

    Writes DecrementOperatorDemo program

---
+++ DecrementOperatorDemo.java
@@ -0,0 +1,14 @@
+class DecrementOperatorDemo{
+public static void main(String[] args){
+System.out.println("Pre Decrement operation----");
+int m=10;
+int n=--m;
+System.out.println("m: "+m);
+System.out.println("n: "+n);
+System.out.println("Post Decrement operation----");
+int a=50;
+int b=a--;
+System.out.println("a: "+a);
+System.out.println("b: "+b);
+}
+}
