952043ccdf2adf7ef4440f012464068fbcc65a13
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-23 20:01:48 +0530

    Writes IncrementOperatorDemoAssignment program

---
+++ IncrementOperatorDemoAssignment.java
@@ -0,0 +1,14 @@
+class IncrementOperatorDemoAssignment{
+public static void main(String[] args){
+System.out.println("Pre increment operation----");
+int m=50;
+int n=++m;
+System.out.println("m: "+m);
+System.out.println("n: "+n);
+System.out.println("Post increment operation----");
+int r=77;
+int s=r++;
+System.out.println("r: "+r);
+System.out.println("s: "+s);
+}
+}
