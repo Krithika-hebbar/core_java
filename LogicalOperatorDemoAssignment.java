32a907938a67da414c4584a30ccd03b370ba0454
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-26 21:58:12 +0530

    LogicalOperatorDemoAssignment program

---
+++ LogicalOperatorDemoAssignment.java
@@ -0,0 +1,10 @@
+class LogicalOperatorDemoAssignment{
+public static void main(String[] args){
+int a=12;
+int b=23;
+System.out.println(a>=b && a!=b);
+System.out.println(a>b && a<=b);
+System.out.println(a==b || a!=b);
+System.out.println(a=!b || a>=b);
+}
+}
