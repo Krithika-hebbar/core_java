ff9333371bcc71baca3bbd2113262a56981972b7
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-26 21:23:16 +0530

    Writes AssignmentOperatorDemoAssignment program

---
+++ AssignmentOperatorDemoAssignment.java
@@ -0,0 +1,12 @@
+class AssignmentOperatorDemoAssignment{
+public static void main(String[] args){
+int m=5;
+int n=70;
+n+=m;
+System.out.println("n: "+n);
+int i=2;
+int j=35;
+i*=j;
+System.out.println("i: "+i);
+}
+}
