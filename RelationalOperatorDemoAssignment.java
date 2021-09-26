0063bf7e254c1b230ef305f566c255687d77eb47
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-26 21:31:10 +0530

    Writes RelationalOperatorDemoAssignment program

---
+++ RelationalOperatorDemoAssignment.java
@@ -0,0 +1,23 @@
+class RelationalOperatorDemoAssignment{
+public static void main(String[] args){
+int i=10;
+int j=46;
+int k=67;
+boolean result=i<k;
+System.out.println("i<k: " +result);
+boolean result1=i>j;
+System.out.println("i>j: " +result1);
+boolean result2=i<=j;
+System.out.println("i<=j: " +result2);
+boolean result3=i>=j;
+System.out.println("j<=i: " +result3);
+boolean result4=i==k;
+System.out.println("i==k: " +result4);
+boolean result5=i>=j;
+System.out.println("i>=j: " +result5);
+boolean result6=i<k;
+System.out.println("i<k: " +result6);
+boolean result7=i!=k;
+System.out.println("i!=k: "  +result7);
+}
+}
