96de8a8dd6f5025060d95c019e5250c4b120190a
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-27 14:03:28 +0530

    Writes TenaryOperatorSmallestDemo program

---
+++ TenaryOperatorSmallestDemo.java
@@ -0,0 +1,8 @@
+class TenaryOperatorSmallestDemo{
+public static void main(String[] args){
+int x=100;
+int y=200;
+int result=x<y ? 100:200;
+System.out.println("smallest of "+x+" & "+y+" is:" +result);
+}
+} 
