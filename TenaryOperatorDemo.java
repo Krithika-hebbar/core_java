2953d88dfeaad48fdcefb10052d0179c789dd449
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-27 13:54:12 +0530

    Writes TenaryOperatorDemo program

---
+++ TenaryOperatorDemo.java
@@ -0,0 +1,8 @@
+class TenaryOperatorDemo{
+public static void main(String[] args){
+int x=100;
+int y=200;
+int result=x>y ? 20:30;
+System.out.println("largest of "+x+" & "+y+" is:" +result);
+}
+} 
