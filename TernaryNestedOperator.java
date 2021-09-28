ac836bb66d2ab5efb8610ee5a24fe92d4313f56d
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-28 11:12:45 +0530

    Writes TernaryNestedOperator program

---
+++ TernaryNestedOperator.java
@@ -0,0 +1,12 @@
+public class TernaryNestedOperator {
+ 
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+ 
+        String result = a > b ? a > c ? "a is greatest" : "c is greatest" : b > c ? "b is greatest" : "c is greatest";
+        System.out.println(result);
+    }
+ 
+}
