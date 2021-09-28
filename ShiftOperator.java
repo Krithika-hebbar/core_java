67534d8e33db823a18179d8b4d923c7d68696ea4
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-09-28 11:08:23 +0530

    Writes ShiftOperator program

---
+++ ShiftOperator.java
@@ -0,0 +1,16 @@
+public class ShiftOperator{
+public static void main(String[] args) {
+int a=2; //10
+int i; 
+i=a>>1; //01
+System.out.println("the value of a before right shfit is: " +a);
+System.out.println("the value of a after applying right shfit is: " +i);
+
+int x=2; //10
+int k;
+k=x<<1; //100
+System.out.println("the value of a before left shift is: " +x);
+System.out.println("the value of a after applying left shift is: " +k);
+	}
+
+}
