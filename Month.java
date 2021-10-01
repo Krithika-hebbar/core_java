49cae223582a3d01439bc9b0da8da54d01c2beb1
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-01 11:57:06 +0530

    Writes Month program

---
+++ Month.java
@@ -0,0 +1,32 @@
+class Month{
+public static void main(String[] args){
+int number=13;
+if(number==1){
+System.out.println("Jan");
+}else if(number==2){
+System.out.println("Feb");
+}else if(number==3){
+System.out.println("Mar");
+}else if(number==4){
+System.out.println("Apr");
+}else if(number==5){
+System.out.println("May");
+}else if(number==6){
+System.out.println("June");
+}else if(number==7){
+System.out.println("July");
+}else if(number==8){
+System.out.println("Aug");
+}else if(number==9){
+System.out.println("Sep");
+}else if(number==10){
+System.out.println("Oct");
+}else if(number==11){
+System.out.println("Nov");
+}else if(number==12){
+System.out.println("Dec");
+}else if(number>12){
+System.out.println("Enter the number between 1 to 12");
+}
+}
+}
