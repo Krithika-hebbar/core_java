1f9fd074c84842251dda8a8e4d16f44095d1f0b7
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-01 12:04:53 +0530

    Writes EvenOddNumber program

---
+++ EvenOddNumber.java
@@ -0,0 +1,12 @@
+class EvenOddNumber{
+public static void main(String[] args){
+int n=3;
+if(n%2==0){
+System.out.println("Number is even");
+}
+else
+{
+System.out.println("Number is odd");
+}
+}
+}
