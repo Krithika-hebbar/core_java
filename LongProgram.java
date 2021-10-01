9d33c82fe8a7295c86a158e6868c119cd8ceb068
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-01 12:02:00 +0530

    Writes LongProgram

---
+++ LongProgram.java
@@ -0,0 +1,11 @@
+class LongProgram{
+public static void main(String[] args){
+int number=9;
+if(number>10){
+System.out.println("Valid number");
+}
+else{
+System.out.println("Inavlid number");
+}
+}
+}
