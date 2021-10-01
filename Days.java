5af7f2cd802d534818b02aed6bca057e65536ad6
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-01 11:58:19 +0530

    Writes Days program

---
+++ Days.java
@@ -0,0 +1,22 @@
+class Days{
+public static void main(String[] args){
+int number=8;
+if(number==1){
+System.out.println("Monday");
+}else if(number==2){
+System.out.println("Tuesday");
+}else if(number==3){
+System.out.println("Wednesday");
+}else if(number==4){
+System.out.println("Thursday");
+}else if(number==5){
+System.out.println("Friday");
+}else if(number==6){
+System.out.println("Saturday");
+}else if(number==7){
+System.out.println("Sunday");
+}else if(number>7){
+System.out.println("Enter the number between 1 to 7");
+}
+}
+}
