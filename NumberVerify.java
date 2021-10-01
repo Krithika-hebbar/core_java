a0733567d2845c668438869ccc6b9ec078ba2cf2
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-01 12:00:11 +0530

    Writes NumberVerify program

---
+++ NumberVerify.java
@@ -0,0 +1,11 @@
+class NumberVerify{
+public static void main(String[] args){
+int number=22;
+if(number>=10 && number<=99){
+System.out.println("Number is 2 digit");
+}
+else{
+System.out.println("Number is more than 2 digit");
+}
+}
+}
