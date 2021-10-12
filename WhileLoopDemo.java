8372ce530dd707d49cf8b0a7258f9e3dbfc05cb8
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-12 21:16:49 +0530

    Writes WhileLoopDemo program

---
+++ WhileLoopDemo.java
@@ -0,0 +1,11 @@
+class WhileLoopDemo{
+public static void main(String[] args){
+System.out.println("Main started");
+int age=18;
+while(age==18){
+ System.out.println("your age is 18");
+ 
+}
+ System.out.println("Main ended");
+}
+}
