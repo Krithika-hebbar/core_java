7d51513edd9f817ed350e2d42d0aadd8225fe62b
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-12 21:27:24 +0530

    Writes NestedforLoopDemo program

---
+++ NestedforLoopDemo.java
@@ -0,0 +1,17 @@
+class NestedforLoopDemo{
+public static void main(String[] args){
+/*for(int i=0;i<3;i++){
+ for(int j=0;j<3;j++){
+  System.out.println(j);
+}
+}
+}
+}*/
+for(int j=0;j<3;j++){
+ System.out.println(j+ " ");
+}
+ System.out.println();
+}
+}
+
+
