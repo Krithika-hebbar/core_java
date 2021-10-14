ce02dc1feee041ef355bb7b8668e1c82eb80e182
Krithika-hebbar <hebbarkrithika99@gmail.com>
2021-10-14 21:21:45 +0530

    Writes findCube program

---
+++ findCube.java
@@ -0,0 +1,9 @@
+class findCube{
+static void findCube(int number){
+int cube=number*number*number;
+System.out.println("cube :"+cube);
+}
+public static void main(String[] args){
+findCube(5);
+}
+}
